c2de1a211c95774274dded6cf5d8863899bce217
Jagadedes <jagadees.pillai@zyxware.com>
2014-11-20 16:59:35 +0530

    31456. removed unwanted bao code

--- CRM/Wci/BAO/ProgressBar.php
+++ CRM/Wci/BAO/ProgressBar.php
@@ -36,99 +36,6 @@ require_once 'CRM/Wci/DAO/ProgressBar.php';
 class CRM_Wci_BAO_ProgressBar extends CRM_Wci_DAO_ProgressBar {
 
   /**
-   * class constructor
-   */
-  function __construct() {
-    parent::__construct();
-  }
-
-  /**
-   * Function to create a ProgressBar
-   * takes an associative array and creates a ProgressBar object
-   *
-   * This function is invoked from within the web form layer and also from the api layer
-   *
-   * @param array   $params      (reference ) an assoc array of name/value pairs
-   *
-   * @return object CRM_Wci_BAO_ProgressBar object
-   * @access public
-   * @static
-   */
-  static function create(array $params) {
-
-    // check required params
-    if (!self::dataExists($params)) {
-      CRM_Core_Error::fatal('Not enough data to create a progress bar.');
-    }
-
-    $progress_bar = new CRM_Wci_BAO_ProgressBar();
-    $progress_bar->copyValues($params);
-
-    $progress_bar->save();
-
-    return $progress_bar;
-  }
-
-  /**
-   * Get a list of Widgets matching the params, where params keys are column
-   * names of civicrm_wci_widget.
-   *
-   * @param array $params
-   * @return array of CRM_Wci_BAO_ProgressBar objects
-   */
-  static function retrieve(array $params) {
-    $result = array();
-
-    $progress_bar = new CRM_Wci_BAO_ProgressBar();
-    $progress_bar->copyValues($params);
-    $progress_bar->find();
-
-    while ($progress_bar->fetch()) {
-      $result[(int) $progress_bar->id] = clone $progress_bar;
-    }
-
-    $progress_bar->free();
-
-    return $result;
-  }
-
-  /**
-   * Wrapper method for retrieve
-   *
-   * @param mixed $id Int or int-like string representing widget ID
-   * @return CRM_Wci_BAO_ProgressBar
-   */
-  static function retrieveByID($id) {
-    if (!is_int($id) && !ctype_digit($id)) {
-      CRM_Core_Error::fatal(__CLASS__ . '::' . __FUNCTION__ . ' expects an integer.');
-    }
-    $id = (int) $id;
-
-    $progress_bars = self::retrieve(array('id' => $id));
-
-    if (!array_key_exists($id, $progress_bars)) {
-      CRM_Core_Error::fatal("No Progress bar with ID $id exists.");
-    }
-
-    return $progress_bars[$id];
-  }
-
-  /**
-   * Check if there is absolute minimum of data to add the object
-   *
-   * @param array  $params         (reference ) an assoc array of name/value pairs
-   *
-   * @return boolean
-   * @access public
-   */
-  public static function dataExists($params) {
-    if (CRM_Utils_Array::value('name', $params)) {
-      return TRUE;
-    }
-    return FALSE;
-  }
-
-  /**
    * Returns array of progressbars
    * Fields : id, name, starting_amount, goal_amount
    * @return progressbar array
@@ -151,9 +58,7 @@ class CRM_Wci_BAO_ProgressBar extends CRM_Wci_DAO_ProgressBar {
   
   /**
    * Returns percentage value of a progressbar
-   *
    * @param integer progressbar id
-   *
    * @return decimal percentage value
    * @access public
    */  
